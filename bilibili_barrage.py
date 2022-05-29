656faec1853efd47cdc970be358048ce52271790
Bloodz <e0548898@u.nus.edu>
2022-05-29 18:16:23 +0800

    [UPDATE] for fix bug in MAC

--- bilibili_barrage.py
+++ bilibili_barrage.py
@@ -17,9 +17,8 @@ import requests
 import word_to_cloud
 
 
-
-#从pagelist里面找到该视频的弹幕文件cid编号，其中pagelist根据视频av号来定位
-def get_cid(av,headers):
+# 从pagelist里面找到该视频的弹幕文件cid编号，其中pagelist根据视频av号来定位
+def get_cid(av, headers):
     page_url = 'https://api.bilibili.com/x/player/pagelist?aid={}&jsonp=jsonp'.format(av)
     response = requests.get(url=page_url, headers=headers).content
     html = json.loads(response)
@@ -28,9 +27,9 @@ def get_cid(av,headers):
 
 
 # 根据cid编号找到弹幕文件
-def get_barrage(cid,headers):
+def get_barrage(cid, headers):
     barrage_url = 'https://api.bilibili.com/x/v1/dm/list.so?oid={}'.format(cid)
-    response = requests.get(url = barrage_url,headers = headers)
+    response = requests.get(url=barrage_url, headers=headers)
     xml = parsel.Selector(response.content.decode('utf-8'))
     barrage_list = xml.xpath('//d').re('<d.*> *(.*)</d>')
 
@@ -45,15 +44,15 @@ def save_file(barrage_list, filename):
             f.write('\n')
 
 
-
-def main(av,headers):
-    cid = get_cid(av,headers)
-    barrage_list = get_barrage(cid,headers)
+def main(av, headers):
+    cid = get_cid(av, headers)
+    barrage_list = get_barrage(cid, headers)
     save_file(barrage_list, '{}.txt'.format(av))
 
 
 if __name__ == '__main__':
     av = input('请输入您想爬取视频的av号，例如86803884等： ')
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-    main(av,headers)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+    main(av, headers)
     word_to_cloud.main(av)
--- word_to_cloud.py
+++ word_to_cloud.py
@@ -12,61 +12,70 @@ Created on 02 07 18:37 2020
 
 """
 
-
 import jieba
 import numpy as np
-from wordcloud import WordCloud,ImageColorGenerator
+from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 import string
 import zhon.hanzi
 import jieba.posseg as pseg
 
-
-
 """读取txt文件"""
+
+
 def open_file(filename):
-    with open(filename,"r",encoding = "utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
     return content
 
 
 """分词"""
+
+
 def word_split(content, txt):
-    #通过词性筛选掉一些无用词
-    anword = [x.word for x in pseg.cut(content) if (x.flag.startswith('a') or x.flag.startswith('n')) and x.word not in txt]
-    return(str(anword))
+    # 通过词性筛选掉一些无用词
+    anword = [x.word for x in pseg.cut(content) if
+              (x.flag.startswith('a') or x.flag.startswith('n')) and x.word not in txt]
+    print(anword)
+    return anword
 
 
 """词云"""
-def word_cloud(wordlist,pic):#pic是图片名称
-    ground = np.array(Image.open("1.jpg"))
-    image_colors = ImageColorGenerator(ground)
-    wc = WordCloud(width = 1600,background_color = "white",max_words = 50,mask = ground,scale = 10,max_font_size = 500,random_state = 42,font_path = "msyh.ttc")
+
+
+def word_cloud(wordlist, pic):  # pic是图片名称
+    # ground = np.array(Image.open("1.jpg"))
+    # image_colors = ImageColorGenerator(ground)
+    wc = WordCloud(width=1600, background_color="white", max_words=50, scale=10, max_font_size=500,
+                   random_state=42, font_path=r'/Users/Bloodz/Library/Fonts/SimHei.ttf')
     wc.generate(wordlist)
     wc.to_file(pic)
-    #plt.imshow(wc,interpolation = "bilinear")
-    #plt.axis("off")
-    #plt.figure(figsize = (100,100))
-    #plt.show()
+    # plt.imshow(wc,interpolation = "bilinear")
+    # plt.axis("off")
+    # plt.figure(figsize = (100,100))
+    # plt.show()
 
 
 """数据清洗"""
-def word_clean(wl,txt):
-    wl = wl.split()
+
+
+def word_clean(wl, txt):
     for i in wl:
         if i.isdigit() or i in txt or i in zhon.hanzi.punctuation or i in string.punctuation:
             wl.remove(i)
-    wl = str(wl).replace("'","")
-    wl = wl.replace(",","")
-    return wl
+    print(wl)
+    return ' '.join(wl)
 
 
 """主程序"""
+
+
 def main(av):
     filename = "{}.txt".format(av)
     content = open_file(filename)
-    word_cloud(word_clean(word_split(content, ""),""),"{}.jpg".format(filename))
+    word_cloud(word_clean(word_split(content, ""), ""), "{}.jpg".format(filename))
+
 
 if __name__ == "__main__":
-    av = 86803884
+    av = 596994126
     main(av)
